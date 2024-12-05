e0afb62dac6f08d301512899758dbe2cc25f19fc
Amar1729 <amar.paul16@gmail.com>
2024-12-05 00:03:15 -0500

    utils (Coord and parse_with_lens) + d04p01

---
+++ rust/d04/src/lib.rs
@@ -0,0 +1,93 @@
+#![allow(dead_code)]
+
+use std::collections::{HashMap, HashSet};
+
+const DIRECTIONS: &[(isize, isize); 8] = &[
+    (1, 0),
+    (1, 1),
+    (0, 1),
+    (-1, 1),
+    (-1, 0),
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+];
+
+fn solve(input: &str, parse: fn(&str) -> u32) -> u32 {
+    parse(input)
+}
+
+fn parse1(input: &str) -> u32 {
+    let (_, it) = utils::parse_with_lens(input, &|b| {b as char});
+
+    let mut map = HashMap::new();
+    let mut start_points = HashSet::new();
+
+    for (coord, letter) in it {
+        // shift the coordinate so i can keep it as usize and still do subtraction.
+        let shifted = (coord.0 + 3, coord.1 + 3);
+
+        map.insert(shifted, letter);
+
+        if letter == 'X' {
+            start_points.insert(shifted);
+        }
+    }
+
+    start_points.iter()
+        .map(|(x, y)| {
+            DIRECTIONS.iter()
+                .map(|(dx, dy)| {
+                    for (d, target) in ['M', 'A', 'S'].iter().enumerate() {
+                        // gross? what am i missing here
+                        let nx = ((d+1) as isize * dx + *x as isize) as usize;
+                        let ny = ((d+1) as isize * dy + *y as isize) as usize;
+
+                        match map.get(&(nx, ny)) {
+                            Some(b) => {
+                                if b != target { return 0; }
+                            },
+                            None => { return 0; }
+                        };
+                    }
+
+                    return 1;
+                })
+                .sum::<u32>()
+        })
+        .sum()
+}
+
+fn parse2(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST1: &'static str = r#"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"#;
+
+    const INPUT: &'static str = include_str!("../input.txt");
+
+    #[test]
+    fn test1() {
+        assert_eq!(solve(TEST1, parse1), 18);
+        assert_eq!(solve(INPUT, parse1), 2464);
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(solve(TEST1, parse2), 0);
+        assert_eq!(solve(INPUT, parse2), 0);
+    }
+}
