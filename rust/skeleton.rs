7e7f434c6ab2f828c4b76d5f1b9b4532d5b0cf91
Amar1729 <amar.paul16@gmail.com>
2024-12-04 23:07:39 -0500

    add skeleton.rs

---
+++ rust/skeleton.rs
@@ -0,0 +1,36 @@
+#[allow(dead_code)]
+
+fn solve(input: &str, parse: fn(&str) -> u32) -> u32 {
+    parse(input)
+}
+
+fn parse1(input: &str) -> u32 {
+    0
+}
+
+fn parse2(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST1: &'static str = r#""#;
+
+    // const TEST2: &'static str = r#""#;
+
+    const INPUT: &'static str = include_str!("../input.txt");
+
+    #[test]
+    fn test1() {
+        assert_eq!(solve(TEST1, parse1), 0);
+        assert_eq!(solve(INPUT, parse1), 0);
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(solve(TEST1, parse2), 0);
+        assert_eq!(solve(INPUT, parse2), 0);
+    }
+}
