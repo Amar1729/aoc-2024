adf877d1e56aa1ccefe20a5c0609d25de8476a28
Amar1729 <amar.paul16@gmail.com>
2024-12-16 00:30:25 -0500

    d07p02

--- rust/d07/src/lib.rs
+++ rust/d07/src/lib.rs
@@ -1,22 +1,30 @@
 #![allow(dead_code)]
 
+use std::iter;
+use itertools::Itertools;
+
 // need u64 for this problem
 fn solve(input: &str, parse: fn(&str) -> u64) -> u64 {
     parse(input)
 }
 
-fn is_valid_line(total: usize, inputs: &[usize]) -> bool {
-    let r: u32 = 2;
-    for bitstring in 0 .. r.pow((inputs.len() - 1) as u32) {
+fn is_valid_line(total: usize, inputs: &[usize], bound: usize) -> bool {
+
+    // a bit annoying that product([], repeat=n) is so verbose
+    for bitstring in iter::repeat_n(0..bound, inputs.len() - 1).multi_cartesian_product() {
 
         let tot = inputs
             .iter()
             .copied()
             .enumerate()
             .reduce(|(_, acc), (op_idx, num)| {
-                match (bitstring >> (op_idx - 1)) & 1 {
+                match bitstring[op_idx - 1] {
                     0 => (0, acc + num),
                     1 => (1, acc * num),
+                    2 => (
+                        2,
+                        (acc.to_string() + &num.to_string()).parse().unwrap(),
+                    ),
                     _ => panic!(),
                 }
             })
@@ -28,7 +36,7 @@ fn is_valid_line(total: usize, inputs: &[usize]) -> bool {
     false
 }
 
-fn parse1(input: &str) -> u64 {
+fn parse_and_solve(input: &str, bound: usize) -> u64 {
     input
         .lines()
         .filter_map(|line| {
@@ -36,7 +44,7 @@ fn parse1(input: &str) -> u64 {
             let total: usize = parts[0].parse().unwrap();
             let nums: Vec<usize> = parts[1].split(' ').map(|n| n.parse().unwrap()).collect();
 
-            if is_valid_line(total, &nums) {
+            if is_valid_line(total, &nums, bound) {
                 Some(total as u64)
             } else {
                 None
@@ -45,8 +53,12 @@ fn parse1(input: &str) -> u64 {
         .sum()
 }
 
+fn parse1(input: &str) -> u64 {
+    parse_and_solve(input, 2)
+}
+
 fn parse2(input: &str) -> u64 {
-    0
+    parse_and_solve(input, 3)
 }
 
 #[cfg(test)]
@@ -67,9 +79,9 @@ mod tests {
 
     #[test]
     fn test_is_valid_line() {
-        assert_eq!(is_valid_line(190, &[10, 19]), true);
-        assert_eq!(is_valid_line(3267, &[81, 40, 27]), true);
-        assert_eq!(is_valid_line(83, &[17, 5]), false);
+        assert_eq!(is_valid_line(190, &[10, 19], 2), true);
+        assert_eq!(is_valid_line(3267, &[81, 40, 27], 2), true);
+        assert_eq!(is_valid_line(83, &[17, 5], 2), false);
     }
 
     #[test]
@@ -80,7 +92,7 @@ mod tests {
 
     #[test]
     fn test2() {
-        assert_eq!(solve(TEST1, parse2), 0);
-        assert_eq!(solve(INPUT, parse2), 0);
+        assert_eq!(solve(TEST1, parse2), 11_387);
+        assert_eq!(solve(INPUT, parse2), 248_427_118_972_289);
     }
 }
