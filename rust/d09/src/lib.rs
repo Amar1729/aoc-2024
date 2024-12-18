8af753a68ccdc02c5e1117dac8c9d45b7abd72b8
Amar1729 <amar.paul16@gmail.com>
2024-12-18 17:40:30 -0500

    d09p02

--- rust/d09/src/lib.rs
+++ rust/d09/src/lib.rs
@@ -22,22 +22,7 @@ fn make_block_map(input: &str) -> Vec<Option<usize>> {
         .collect()
 }
 
-fn parse1(input: &str) -> u64 {
-    let mut blocks = make_block_map(input);
-
-    let mut small = 0;
-    let mut big = blocks.len();
-
-    while small < big {
-        small = blocks.iter().position(|e| e.is_none()).unwrap();
-        big = blocks.len() - blocks.iter().rev().position(|e| e.is_some()).unwrap() - 1;
-
-        if small >= big { break }
-
-        blocks[small] = blocks[big];
-        blocks[big] = None;
-    }
-
+fn block_checksum(blocks: &[Option<usize>]) -> u64 {
     blocks
         .iter()
         .enumerate()
@@ -50,8 +35,74 @@ fn parse1(input: &str) -> u64 {
         .sum()
 }
 
+fn print_blocks(blocks: &[Option<usize>]) {
+    for b in blocks {
+        match b {
+            Some(b) => print!("{b}"),
+            None => print!("."),
+        };
+    }
+    println!();
+}
+
+fn parse1(input: &str) -> u64 {
+    let mut blocks = make_block_map(input);
+
+    let mut small = 0;
+    let mut big = blocks.len();
+
+    while small < big {
+        // inefficient, but whatever
+        small = blocks.iter().position(|e| e.is_none()).unwrap();
+        big = blocks.len() - blocks.iter().rev().position(|e| e.is_some()).unwrap() - 1;
+
+        if small >= big { break }
+
+        blocks[small] = blocks[big];
+        blocks[big] = None;
+    }
+
+    block_checksum(&blocks)
+}
+
 fn parse2(input: &str) -> u64 {
-    0
+    let mut blocks = make_block_map(input);
+
+    let mut big = blocks.len() - blocks.iter().rev().position(|e| e.is_some()).unwrap() - 1;
+
+    // avoiding off-by-one errors during this was a real pain
+    while big > 0 {
+        let mut big_range = 1;
+        while big > big_range && blocks[big - big_range + 1] == blocks[big] {
+            big_range += 1;
+        }
+        if big <= big_range {
+            break;
+        }
+        big_range -= 1;
+
+        let ok_space = (1 .. big - big_range)
+            .find(|small| {
+                (0 .. big_range)
+                    .all(|idx| blocks[small + idx].is_none())
+            });
+
+        if let Some(small) = ok_space {
+            let label = blocks[big].unwrap();
+            for idx in 0 .. big_range {
+                blocks[small + idx] = Some(label);
+                blocks[big - idx] = None;
+            }
+        }
+
+        big -= big_range;
+        while blocks[big].is_none() && big > 0 {
+            big -= 1;
+        }
+    }
+
+    // print_blocks(&blocks);
+    block_checksum(&blocks)
 }
 
 #[cfg(test)]
@@ -70,7 +121,7 @@ mod tests {
 
     #[test]
     fn test2() {
-        assert_eq!(solve(TEST1, parse2), 0);
-        assert_eq!(solve(INPUT, parse2), 0);
+        assert_eq!(solve(TEST1, parse2), 2858);
+        assert_eq!(solve(INPUT, parse2), 6398096697992);
     }
 }
