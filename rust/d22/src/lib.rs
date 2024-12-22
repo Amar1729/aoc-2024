6a557143305b0b783fda6658bf144744a8a17fdf
Amar1729 <amar.paul16@gmail.com>
2024-12-22 05:05:05 -0500

    d22p02

--- rust/d22/src/lib.rs
+++ rust/d22/src/lib.rs
@@ -1,5 +1,8 @@
 #![allow(dead_code)]
 
+use itertools::Itertools;
+use std::collections::{HashMap, HashSet, VecDeque};
+
 fn solve(input: &str, parse: fn(&str) -> u64) -> u64 {
     parse(input)
 }
@@ -37,7 +40,46 @@ fn parse1(input: &str) -> u64 {
 }
 
 fn parse2(input: &str) -> u64 {
-    0
+    let mut summed = HashMap::new();
+
+    // TODO: profile this? runs ~slowly (~4.7s).
+    for line in input.lines() {
+        // experimentally determined (slightly helps runtime, avoids lots small memallocs)
+        let mut seen = HashSet::with_capacity(3584);
+        let mut sec = line.parse().unwrap();
+        let mut prev = sec % 10;
+        let mut offsets = VecDeque::with_capacity(3);
+        for idx in 0 .. 2000 {
+            sec = evolve(sec);
+            let digit = sec % 10;
+            if idx > 2 {
+                let leadup = (
+                    offsets[0],
+                    offsets[1],
+                    offsets[2],
+                    digit as isize - prev as isize,
+                );
+
+                if !seen.contains(&leadup) {
+                    seen.insert(leadup);
+                    *summed.entry(leadup).or_insert(0) += digit as usize;
+                }
+
+                offsets.pop_front();
+            }
+            offsets.push_back(digit as isize - prev as isize);
+            prev = sec % 10;
+        }
+    }
+
+    let result = summed
+        .iter()
+        // sort in descending order, by number of bananas of this sequence
+        .sorted_by(|a, b| Ord::cmp(b.1, a.1))
+        .next()
+        .unwrap();
+
+    *result.1 as u64
 }
 
 #[cfg(test)]
@@ -47,6 +89,12 @@ mod tests {
     const TEST1: &'static str = r#"1
 10
 100
+2024"#;
+
+    // sneaky aoc writers swapping out the example on us
+    const TEST2: &'static str = r#"1
+2
+3
 2024"#;
 
     const INPUT: &'static str = include_str!("../input.txt");
@@ -64,7 +112,7 @@ mod tests {
 
     #[test]
     fn test2() {
-        assert_eq!(solve(TEST1, parse2), 0);
-        assert_eq!(solve(INPUT, parse2), 0);
+        assert_eq!(solve(TEST2, parse2), 23);
+        assert_eq!(solve(INPUT, parse2), 2399);
     }
 }
